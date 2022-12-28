e0e4dc96eb21ef29a384cbd75aa9faf52078ab05
Ilias-Karageorgos <118739697+Ilias-Karageorgos@users.noreply.github.com>
2022-12-28 20:12:28 +0200

    Create Functions.js

---
+++ print-negative/Functions.js
@@ -0,0 +1,13 @@
+function makeNegative(num) {
+    
+    if (num > 0) {
+        num = -num;
+        }
+    else if (num <0 ) {
+        num = num;
+    }
+    else if (num == 0) {
+        num = 0;
+    }
+    return num;
+}
