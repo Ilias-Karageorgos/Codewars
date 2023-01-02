65103b507c7fd31eb099456640e2c8c1b92e50ea
Ilias-Karageorgos <118739697+Ilias-Karageorgos@users.noreply.github.com>
2023-01-02 06:27:35 +0200

    Create Functions.js

---
+++ reversed-strings/Functions.js
@@ -0,0 +1,11 @@
+function solution(str) {
+    let result = "";
+    
+    for (var i = str.length - 1; i >= 0; i--) {
+      
+             
+        result = result + str[i];
+    }
+    
+    return result;
+}
